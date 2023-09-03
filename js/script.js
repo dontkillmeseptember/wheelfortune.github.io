e5822f5c6e5605cc4b5c9f4525f4880592989c2b
Spurs <raykage1337@gmail.com>
2023-09-03 12:29:42 +0300

    modified:   css/styles.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -30,6 +30,22 @@ const sectors = [
 	{
 		color: '#F54747',
 		label: 'Индия'
+	},
+	{
+		color: '#F6B66C',
+		label: 'Слава',
+	},
+	{
+		color: '#F54747',
+		label: 'Нейросеть',
+	},
+	{
+		color: '#F6B66C',
+		label: 'Швеция'
+	},
+	{
+		color: '#F54747',
+		label: 'Испания'
 	}
 ]
 
@@ -38,6 +54,7 @@ const tot = sectors.length
 const spinEl = document.querySelector('#spin')
 const ctx = document.querySelector('#wheel').getContext('2d')
 const finalValue = document.getElementById("final-value")
+const story = document.getElementById("story")
 const dia = ctx.canvas.width
 const rad = dia / 2
 const PI = Math.PI
@@ -45,13 +62,17 @@ const TAU = 2 * PI
 const arc = TAU / sectors.length
 
 const friction = 0.991 // 0,995 = Мягкий, 0,99 = Средний, 0,98 = Жесткий
+
 let angVel = 0 // Угловая скорость
 let ang = 0 // Угол в радиусе
+let spinning = false;
+let timer;
 
 const getIndex = () => Math.floor(tot - (ang / TAU) * tot) % tot
 
 function drawSector(sector, i) {
 	const ang = arc * i
+
 	ctx.save()
 	// Цвет
 	ctx.beginPath()
@@ -74,20 +95,41 @@ function drawSector(sector, i) {
 // Функция нажатия на SPIN
 function rotate() {
 	const sector = sectors[getIndex()]
+	const selectedLabel = sector.label;
+
 	ctx.canvas.style.transform = `rotate(${ang - PI / 2}rad)`
 	spinEl.textContent = !angVel ? 'СТАРТ' : 'СТАРТ'
 	spinEl.style.background = sector.color
-	finalValue.innerHTML = `<p>Следующий рацион из страны: ${sector.label}</p>`
+
+	// Добавьте условный оператор для изменения сообщения в story
+	if (selectedLabel === 'Нейросеть' || selectedLabel === 'Слава') {
+		story.innerHTML = `Следующий рацион делает: ${selectedLabel}`;
+	} else {
+		story.innerHTML = `Следующий рацион из страны: ${selectedLabel}`;
+	}
+
+	clearTimeout(timer);
+	timer = setTimeout(() => {
+	  spinning = false; // После завершения таймера можно снова вращать
+	  spinEl.textContent = 'СТАРТ';
+	  spinEl.style.background = sector.color;
+	}, 5); // 10000 миллисекунд (10 секунд) таймер
+
+	// Сохраняем значение в localStorage
+	localStorage.setItem('selectedLabel', selectedLabel);
+
+	spinning = true; // Устанавливаем флаг в true, чтобы предотвратить повторное вращение
 }
 
 // Функция движения колеса
 function frame() {
-	if (!angVel) return
-	angVel *= friction // Снижение скорости за счет трения
-	if (angVel < 0.002) angVel = 0 // Привести к остановке
-	ang += angVel // Угол обновления
-	ang %= TAU // Нормализация угла
-	rotate()
+	if (!angVel) return;
+	angVel *= friction;
+	if (angVel < 0.002) angVel = 0;
+	ang += angVel;
+	finalValue.innerHTML = "Удачи!⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
+	ang %= TAU;
+	rotate();
 }
 
 // Функция движка
@@ -98,14 +140,26 @@ function engine() {
 
 // Функция запуска всех функций при нажатиии на Spin
 function init() {
+	const storedLabel = localStorage.getItem('selectedLabel');
+
+	ctx.font = '17px Lobster'
+
 	sectors.forEach(drawSector)
 	rotate() // Начальное вращение
+
+	finalValue.innerHTML = "Нажмите на кнопку Старт, чтобы начать";
+
+	if (storedLabel) {
+		story.innerHTML = `Следующий рацион из страны: ${storedLabel}`;
+	}
+
 	engine() // Запуск
+
 	spinEl.addEventListener('click', () => {
-		if (!angVel) angVel = rand(0.25, 0.45)
-		
-		finalValue.innerHTML = `<p>Удачи!</p>`
-	})
+		if (!angVel && !spinning) {
+			angVel = rand(0.25, 0.45);
+		}
+	});
 }
 
 init()
